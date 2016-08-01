a919b54d6a06629d3af8882f8d489269cf1e4f88
Thai Long Tran <long@boldradius.com>
2016-08-01 12:15:37 -0400

    more slides

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 scalaVersion := "2.10.6"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" % "spark-core_2.10" % "2.0.0-preview",
-  "org.apache.spark" % "spark-streaming_2.10" % "2.0.0-preview",
-  "org.apache.spark" % "spark-sql_2.10" % "2.0.0-preview"
+  "org.apache.spark" % "spark-core_2.10" % "2.0.0",
+  "org.apache.spark" % "spark-streaming_2.10" % "2.0.0",
+  "org.apache.spark" % "spark-sql_2.10" % "2.0.0"
 )
 
--- src/main/scala/StructuredStreamDemo.scala
+++ src/main/scala/StructuredStreamDemo.scala
@@ -2,16 +2,46 @@ import org.apache.spark.sql.SparkSession
 
 
 class StructuredStreamDemo {
-  val sparkSession = SparkSession.builder
+  /*val sparkSession = SparkSession.builder
     .master("local")
     .appName("my-spark-app")
     .getOrCreate()
   // .config("spark.some.config.option", "config-value")
 
   //sparkSession.read.stream()
-  val all = sparkSession.read.format("json").stream("data/stocks.json")
+ // val all = sparkSession.read.format("json").stream("data/stocks.json")
 
 
   //is there a structured stream for twitter
   //how to ad-hoc query twitter?
+*/
+  import org.apache.spark.sql.functions._
+  import org.apache.spark.sql.SparkSession
+
+  val spark = SparkSession
+    .builder
+    .appName("StructuredNetworkWordCount")
+    .getOrCreate()
+
+  import spark.implicits._
+  // Create DataFrame representing the stream of input lines from connection to localhost:9999
+  val lines = spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", 9999)
+    .load()
+
+  // Split the lines into words
+  val words = lines.as[String].flatMap(_.split(" "))
+
+  // Generate running word count
+  val wordCounts = words.groupBy("value").count()
+
+  // Start running the query that prints the running counts to the console
+  val query = wordCounts.writeStream
+    .outputMode("complete")
+    .format("console")
+    .start()
+
+  query.awaitTermination()
 }
